fce8bd5f68dcac8263b37b85b40ab485b6d47eeb
Lazare Rossillon <lazrossi@student.42.fr>
2024-11-04 15:14:31 +0100

    really use autoform

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
+import { AutoForm } from "@autoform/mui";
+import { ZodProvider } from "@autoform/zod";
 import React from "react";
 import { z } from "zod";
-import { AutoForm } from "./components/ui/auto-form";
 
 const userSchema = z.object({
   name: z.string().min(2, "Name must be at least 2 characters"),
@@ -8,17 +9,20 @@ const userSchema = z.object({
   age: z.number().min(18, "Must be at least 18 years old"),
 });
 
+const schemaProvider = new ZodProvider(userSchema);
+
 function App() {
   console.log("React Version:", React.version);
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">User Form</h1>
       <AutoForm
-        schema={userSchema}
+        schema={schemaProvider}
         onSubmit={(data) => {
           console.log(data);
           alert("Form submitted! Check console for data");
         }}
+        withSubmit
       />
     </div>
   );
